d39c49dcc043a22cd6c22fe147abb4c6ee82ce50
Olivier Lukas <lukas.olivier@student.howest.be>
2023-12-30 19:46:28 +0100

    adjustment for CORS

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # Import the CORS extension
 import random
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Sample initial quotes
 quotes = ["Reach for the sun!", "Start where the rest stops!", "Learn, code, repeat."]
