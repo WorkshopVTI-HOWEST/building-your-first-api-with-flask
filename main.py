a4ace875fe7653e377648c0f80ce0edd0e08097a
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-01-24 11:53:02 +0100

    finished

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS 
+from flask_cors import CORS  # Import the CORS extension
 import random
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Enable CORS for all routes
 
+# Sample initial quotes
+quotes = ["Reach for the sun!", "Start where the rest stops!", "Learn, code, repeat."]
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/quotes', methods=['GET'])
+def get_quotes():
+    return jsonify({'quotes': quotes})
 
-### Jullie code hieronder ###
+@app.route('/quote', methods=['GET'])
+def get_random_quote():
+    random_quote = random.choice(quotes)
+    return jsonify({'quote': random_quote})
+
+@app.route('/quotes', methods=['POST'])
+def add_quote():
+    data = request.get_json()
+
+    if 'quote' in data:
+        new_quote = data['quote']
+        quotes.append(new_quote)
+        return jsonify({'message': 'Quote added successfully'})
+    else:
+        return jsonify({'error': 'Invalid request'}), 400
 
 
-### Jullie code hierboven ###
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
